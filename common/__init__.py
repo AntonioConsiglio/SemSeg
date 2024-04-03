978b99db945993f35a12ffacab31d847fd8b631b
Antonio Consiglio <consiglio.antonio.bi@gmail.com>
2024-04-03 16:04:15 +0000

    set random seed for all

--- common/__init__.py
+++ common/__init__.py
@@ -6,6 +6,8 @@ from common.logger import TrainLogger
 def set_all_seeds(seed=12345):
     import torch
     import numpy
+    import random
 
+    random.seed(seed)
     numpy.random.seed(seed)
     torch.manual_seed(seed)
